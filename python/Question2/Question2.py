57528ea6feb03ae87ba6392d8e840a630b70f360
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 11:40:41 -0400

    Commited by GCC-Challenge-2020

--- python/Question2/Question2.py
+++ python/Question2/Question2.py
@@ -1,7 +1,29 @@
 # You may change this function parameters
 def findMaxProfit(numOfPredictedTimes, predictedSharePrices):
     # Participants code will be here
-    return -1
+    maxi = 0  
+    n = numOfPredictedTimes
+    for k in range(2, n):
+        profit = [[0 for i in range(k + 1)] 
+                     for j in range(n)] 
+          
+        # Profit is zero for the first 
+        # day and for zero transactions 
+        for i in range(1, n): 
+              
+            for j in range(1, k + 1): 
+                max_so_far = 0
+                  
+                for l in range(i): 
+                    max_so_far = max(max_so_far, predictedSharePrices[i] -
+                                predictedSharePrices[l] + profit[l][j - 1]) 
+                                  
+                profit[i][j] = max(profit[i - 1][j], max_so_far) 
+          
+        if profit[n - 1][k] >= maxi:
+            maxi = profit[n - 1][k]
+        
+    return maxi
 
 def main():
     line = input().split()

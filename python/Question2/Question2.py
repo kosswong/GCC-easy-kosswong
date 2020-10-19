468379d8973529dd1384b47b342bf0752dad22f0
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-19 13:57:18 -0400

    Commited by GCC-Challenge-2020

--- python/Question2/Question2.py
+++ python/Question2/Question2.py
@@ -1,26 +1,23 @@
 # You may change this function parameters
-def findMaxProfit(numOfPredictedTimes, predictedSharePrices):
-    # Participants code will be here
-    maxi = 0   
-    n = numOfPredictedTimes
-    for k in range(1, n):
-        profit = [[0 for i in range(n + 1)]  
-                     for j in range(k + 1)]  
-      
-        # Fill the table in bottom-up fashion  
-        for i in range(1, k + 1):  
-            prevDiff = float('-inf') 
-            for j in range(1, n):  
-                prevDiff = max(prevDiff, 
-                               profit[i - 1][j - 1] - 
-                               predictedSharePrices[j - 1])  
-                profit[i][j] = max(profit[i][j - 1],  
-                                   predictedSharePrices[j] + prevDiff)  
+def findMaxProfit(numOfPredictedTimes, predictedSharePrices):  
   
-        if profit[k][n - 1] >= maxi:
-            maxi = profit[k][n - 1]
-        
-    return maxi
+    k = numOfPredictedTimes-1
+    
+    profit = [[0 for i in range(numOfPredictedTimes + 1)]  
+                 for j in range(k + 1)]  
+  
+    # Fill the table in bottom-up fashion  
+    for i in range(1, k + 1):  
+        prevDiff = float('-inf') 
+          
+        for j in range(1, numOfPredictedTimes):  
+            prevDiff = max(prevDiff, 
+                           profit[i - 1][j - 1] - 
+                           predictedSharePrices[j - 1])  
+            profit[i][j] = max(profit[i][j - 1],  
+                               predictedSharePrices[j] + prevDiff)  
+  
+    return profit[k][numOfPredictedTimes - 1]  
 
 def main():
     line = input().split()

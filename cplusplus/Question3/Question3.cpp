70277661fa1065ae755387c66ff2a18a002fb086
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-24 14:14:02 +0100

    Commited by GCC-Challenge-2020

--- cplusplus/Question3/Question3.cpp
+++ cplusplus/Question3/Question3.cpp
@@ -1,10 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-string find_min_days(int profit[], int price[])
+string find_min_days(int profit[], int price[], int n, int d)
 {
-	//Participants code will be here
-    return "";
-
+	//Participants will add code here
+        string stringToReturn = "";
+        for(int k = 0; k < d; k++){
+            int iOfMinj = 0;
+            int minj = 100001;
+            for(int i = 0; i < n; i++){
+                for(int j = i+1; j < n; j++){
+                    if(price[j]-price[i] == profit[k]){
+                        if(j <= minj){
+                            iOfMinj = i;
+                            minj = j;
+                        }
+                    }
+                }
+            }
+            if(minj != 100001){
+                if(k > 0)
+                    stringToReturn = stringToReturn + ",";
+                stringToReturn = stringToReturn + to_string(iOfMinj+1) + " " + to_string(minj+1);
+            }else{
+                if(k > 0) 
+                    stringToReturn = stringToReturn + ",";
+                stringToReturn = stringToReturn + "-1";
+            }
+        }
+        return stringToReturn;
 }
 
 int main ()
@@ -18,7 +41,7 @@ int main ()
 		cin>>price[i];
 	for (i=0;i<d;i++)
 	    cin>>profit[i];
-    answer = find_min_days(profit,price);
+    answer = find_min_days(profit,price,n,d);
 
     // Do not remove below line
 	cout<<answer<<endl;
